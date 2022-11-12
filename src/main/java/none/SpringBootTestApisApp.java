47844ce722e87ec3d6565769b67748d28dcec159
monthebrice2000 <65128522+monthebrice2000@users.noreply.github.com>
2022-11-12 14:47:43 +0100

    Initial version of spring-boot-test-apis generated by generator-jhipster@7.9.3

---
+++ src/main/java/none/SpringBootTestApisApp.java
@@ -0,0 +1,12 @@
+package none;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApisApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestApisApp.class, args);
+    }
+}
